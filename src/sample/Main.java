2984c0db2aa4c40815a311ab9cc38dc65ff93964
melodymoselle <mmoselle@gmail.com>
2017-10-04 15:03:59 -0700

    adding scene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Label label = new Label("Hello World, JavaFZ!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        
         primaryStage.show();
     }
 
